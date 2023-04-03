a71f42025863d8d1fd3ad44b52c4a32b634f8b41
jarnevermant <r0836442@student.thomasmore.be>
2023-04-03 22:58:16 +0200

    Attempt to fix vault gear piece textures

--- src/components/pages/gear/GearPiece/GearPiece.js
+++ src/components/pages/gear/GearPiece/GearPiece.js
@@ -4,6 +4,16 @@ import './GearPiece.scss';
 // Components
 import IconDisplayMedium from '../../../icon-display/medium/IconDisplayMedium';
 
+import gearPiece_helmet from '../../../../images/assets/the_vault/item/gear/helmet.png';
+import gearPiece_chestplate from '../../../../images/assets/the_vault/item/gear/chestplate.png';
+import gearPiece_leggings from '../../../../images/assets/the_vault/item/gear/leggings.png';
+import gearPiece_boots from '../../../../images/assets/the_vault/item/gear/boots.png';
+import gearPiece_sword from '../../../../images/assets/the_vault/item/gear/sword.png';
+import gearPiece_axe from '../../../../images/assets/the_vault/item/gear/axe.png';
+import gearPiece_idol from '../../../../images/assets/the_vault/item/gear/idol.png';
+import gearPiece_shield from '../../../../images/assets/the_vault/item/gear/shield.png';
+import gearPiece_magnet from '../../../../images/assets/the_vault/item/gear/magnet.png';
+
 
 function GearPiece(props) {
 
@@ -11,7 +21,7 @@ function GearPiece(props) {
         isOddChild
     } = props
 
-    const imageURL = require(`../../../../images/assets/the_vault/item/gear/${props.name}.png`);
+    const imageURL = import(`../../../../images/assets/the_vault/item/gear/${props.name}.png`);
 
     return (
         <div className='gear-piece'>
