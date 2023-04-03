2dc10219a09dcb3e03c764967c5122cbb0fe1c67
jarnevermant <r0836442@student.thomasmore.be>
2023-04-03 17:13:01 +0200

    Insanity has hit and i'm nearly accepting defeat

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,7 @@ module.exports = {
             options: {
               name: '[name].[ext]',
               outputPath: 'images',
-              publicPath: '/images',
-              context: path.resolve(__dirname, 'src')
+              publicPath: './images'
             }
           }
         ]
