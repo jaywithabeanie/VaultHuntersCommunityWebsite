9bdd6967600b9c7d609ae3b0104eb88232513e10
jarnevermant <r0836442@student.thomasmore.be>
2023-04-11 09:34:22 +0200

    Attempt at copying over src folder containing images

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -14,7 +15,12 @@ module.exports = {
       template: './public/index.html',
       filename: 'index.html', // specify the output file name
       path: path.resolve(__dirname, 'build'), // specify the output directory
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/images', to: 'src/images' },
+      ],
+    }),
   ],
   module: {
     rules: [
