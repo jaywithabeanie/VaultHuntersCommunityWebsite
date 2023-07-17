d6975f67724e9edc78b9198c5ff7a2461615b4d2
RSNFreud <23102186+RSNFreud@users.noreply.github.com>
2023-07-17 21:21:11 +0300

    build(deps): update package-lock.json and Gear.js

--- src/components/pages/gear/Gear.js
+++ src/components/pages/gear/Gear.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Gear.scss';
 
 // Components
@@ -15,7 +15,7 @@ import lang from '../../../data/lang/vault_gear.json';
 
 const gearPieces = ['sword', 'axe', 'helmet', 'chestplate', 'leggings', 'boots', 'idol_benevolent', 'shield', 'wand', 'magnet', 'jewel'];
 
-function Home () {
+function Home() {
 
   const [selectedGearPiece, setSelectedGearPiece] = useState(gearPieces[0]);
   const [level, setLevel] = useState(0);
@@ -29,7 +29,7 @@ function Home () {
   };
 
   function getTotalWeight(modifierGroup, modifiers) {
-    
+
     // Initiate variables
     var totalWeight = 0;
 
@@ -46,16 +46,16 @@ function Home () {
         && !(modifierGroup === 'IMPLICIT' && getExcludingModifiers(modifiers, modifier).length === 0)
         && modifier.tiers.some((tier) => (tier.minLevel <= level && level <= (tier.maxLevel < 0 ? 100 : tier.maxLevel)))) {
 
-          // Initiate variables
-          const availableTiers = modifier.tiers.filter(tier => tier.minLevel <= level && (tier.maxLevel === -1 || tier.maxLevel >= level));
+        // Initiate variables
+        const availableTiers = modifier.tiers.filter(tier => tier.minLevel <= level && (tier.maxLevel === -1 || tier.maxLevel >= level));
 
-          // Loop through tiers
-          availableTiers.map((tier) => {
-            
-            // Increase weight
-            totalWeight += tier.weight;
-    
-          })
+        // Loop through tiers
+        availableTiers.map((tier) => {
+
+          // Increase weight
+          totalWeight += tier.weight;
+
+        })
 
       }
 
@@ -84,6 +84,14 @@ function Home () {
       "color": "#ffffff"
     };
 
+    if (modifier.identifier.includes("base_added_ability_level")) {
+      return modifierData = {
+        "name": "+1 to an ability",
+        "description": "Adds a level to an ability",
+        "color": "#c1579d"
+      }
+    }
+
     // Check if attribute exists
     if (lang.vault_gear.modifiers[modifier.attribute] != null) {
       modifierData = lang.vault_gear.modifiers[modifier.attribute];
@@ -92,7 +100,7 @@ function Home () {
     if (lang.vault_gear.modifiers[modifier.identifier] != null) {
       modifierData = lang.vault_gear.modifiers[modifier.identifier];
     }
-    
+
     return modifierData;
 
   }
@@ -121,64 +129,74 @@ function Home () {
       .map(otherModifier => getModifierData(otherModifier).name)
   }
 
+  // brrrr
+  const removeDuplicateLevels = (modifiers) => {
+    const arr = []
+    modifiers.map(e => {
+      if (e.attribute === 'the_vault:added_ability_level' && arr.filter(e => e.attribute === 'the_vault:added_ability_level').length) return
+      arr.push(e)
+    })
+    return arr
+  }
+
   return (
     <>
-      <Title icon={chestplate} title='Vault Gear'/>
+      <Title icon={chestplate} title='Vault Gear' />
 
       <div className='gear-pieces'>
         {gearPieces.map((gearPiece, index) => (
           <label hoverable="true" key={index} htmlFor={`gear-piece-${index}`}>
             <input
-            id={`gear-piece-${index}`}
-            type='radio'
-            name='gear'
-            value={gearPiece}
-            checked={selectedGearPiece === gearPiece}
-            onChange={handleGearPieceChange} />
-            <GearPiece name={gearPiece} isOddChild={index % 2 === 0}/>
+              id={`gear-piece-${index}`}
+              type='radio'
+              name='gear'
+              value={gearPiece}
+              checked={selectedGearPiece === gearPiece}
+              onChange={handleGearPieceChange} />
+            <GearPiece name={gearPiece} isOddChild={index % 2 === 0} />
           </label>
         ))}
       </div>
-      
+
       <Content>
 
         <div className="content-title">
           <h2>{`
             ${getGearPieceDisplayName(selectedGearPiece)}
           `}</h2>
-          <Slider onChange={handleLevelSliderChange}/>
+          <Slider onChange={handleLevelSliderChange} />
         </div>
 
         {Object.entries(getGearPieceModifierData().modifierGroup).map(([modifierGroup, modifiers]) => (
           <>
             {
-            getModifierGroupDisplayName(modifierGroup) !== "Unknown"
+              getModifierGroupDisplayName(modifierGroup) !== "Unknown"
               && modifiers.length > 0
               && modifiers.some((modifier) => modifier.tiers.some((tier) => tier.minLevel <= level))
               && (
-            <div key={modifierGroup} className='gear-modifier-group'>
-              <h3>{getModifierGroupDisplayName(modifierGroup)}</h3>
-              {['PREFIX', 'SUFFIX'].includes(modifierGroup) && 
-                <p>A gear piece with an odd amount of modifier slots has a 50% chance at rolling either a prefix or suffix.</p>
-              }
-              {modifiers.map((modifier) => (
-                <>
-                  {modifier.tiers.length > 0
-                    && modifier.tiers.some((tier) => (tier.minLevel <= level && level <= (tier.maxLevel < 0 ? 100 : tier.maxLevel) && tier.weight > 0)) && (
-                    <Modifier 
-                      key={modifier.identifier}
-                      gearPiece={selectedGearPiece}
-                      modifierGroup={modifierGroup}
-                      modifier={modifier}
-                      langData={getModifierData(modifier)}
-                      level={level}
-                      excludingModifiers={getExcludingModifiers(modifiers, modifier)}
-                      totalWeight={getTotalWeight(modifierGroup, modifiers)}
-                    />
-                  )}
-                </>
-              ))}
-            </div>)}
+                <div key={modifierGroup} className='gear-modifier-group'>
+                  <h3>{getModifierGroupDisplayName(modifierGroup)}</h3>
+                  {['PREFIX', 'SUFFIX'].includes(modifierGroup) &&
+                    <p>A gear piece with an odd amount of modifier slots has a 50% chance at rolling either a prefix or suffix.</p>
+                  }
+                  {removeDuplicateLevels(modifiers).map((modifier) => (
+                    <>
+                      {modifier.tiers.length > 0
+                        && modifier.tiers.some((tier) => (tier.minLevel <= level && level <= (tier.maxLevel < 0 ? 100 : tier.maxLevel) && tier.weight > 0)) && (
+                          <Modifier
+                            key={modifier.identifier}
+                            gearPiece={selectedGearPiece}
+                            modifierGroup={modifierGroup}
+                            modifier={modifier}
+                            langData={getModifierData(modifier)}
+                            level={level}
+                            excludingModifiers={getExcludingModifiers(removeDuplicateLevels(modifiers), modifier)}
+                            totalWeight={getTotalWeight(modifierGroup, modifiers)}
+                          />
+                        )}
+                    </>
+                  ))}
+                </div>)}
           </>
         ))}
 
