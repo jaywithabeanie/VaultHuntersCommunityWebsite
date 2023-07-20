b97341505408b3aab485235086ec4700b24506bc
rsnfreud <theo@freud-online.co.uk>
2023-07-20 17:41:37 +0300

    update functions.tsx and page.tsx

--- src/app/gear/functions.tsx
+++ src/app/gear/functions.tsx
@@ -1,73 +1,141 @@
-import { GearType, ModifierType, groupTypes } from "./page"
 import data from "@/data/vault_gear.json";
 
-export const removeDuplicateLevels = (modifiers: ModifierType[]) => {
-    const arr: ModifierType[] = []
-    const addedMods: string[] = []
-    modifiers.map(e => {
-        if (e.attribute === 'the_vault:added_ability_level') {
-            const levelFilter = arr.filter(e => e.attribute === 'the_vault:added_ability_level')
-            if (levelFilter.length) {
-                if (addedMods && addedMods.filter(q => q === e.identifier).length) return
-                addedMods.push(e.identifier)
-                levelFilter[0].tiers = levelFilter[0].tiers.map((q, count) => ({ ...q, weight: q.weight + e.tiers[count].weight }))
-                return
-            }
-            return arr.push(e)
-        }
-        arr.push(e)
-    })
+export const gearPieces = [
+  "sword",
+  "axe",
+  "helmet",
+  "chestplate",
+  "leggings",
+  "boots",
+  "idol_benevolent",
+  "shield",
+  "wand",
+  "magnet",
+  "jewel",
+] as const;
+export const groupTypes = [
+  "PREFIX",
+  "SUFFIX",
+  "IMPLICIT",
+  "CRAFTED_PREFIX",
+  "CRAFTED_SUFFIX",
+] as const;
 
-    return arr
-}
+export type TierType = {
+  minLevel: number;
+  maxLevel: number;
+  weight: number;
+  value: {
+    min: number;
+    max: number;
+    step: number;
+    levelChange?: number;
+    minChance?: number;
+    maxChance?: number;
+    tooltipDisplayName?: string;
+    registryKey?: string;
+  };
+};
+
+export type ModifierType = {
+  attribute: string;
+  group: string;
+  identifier: string;
+  tiers: TierType[];
+};
+
+export type GearType = {
+  modifierGroup: {
+    IMPLICIT: ModifierType[];
+    PREFIX: ModifierType[];
+    SUFFIX: ModifierType[];
+    CRAFTED_PREFIX: ModifierType[];
+    CRAFTED_SUFFIX: ModifierType[];
+  };
+};
+
+export const removeDuplicateLevels = (modifiers: ModifierType[]) => {
+  const arr: ModifierType[] = [];
+  const addedMods: string[] = [];
+  modifiers.map((e) => {
+    if (e.attribute === "the_vault:added_ability_level") {
+      const levelFilter = arr.filter(
+        (e) => e.attribute === "the_vault:added_ability_level"
+      );
+      if (levelFilter.length) {
+        if (addedMods && addedMods.filter((q) => q === e.identifier).length)
+          return;
+        addedMods.push(e.identifier);
+        levelFilter[0].tiers = levelFilter[0].tiers.map((q, count) => ({
+          ...q,
+          weight: q.weight + e.tiers[count].weight,
+        }));
+        return;
+      }
+      return arr.push(e);
+    }
+    arr.push(e);
+  });
+
+  return arr;
+};
 
 export const getGearPieceModifierData = (selectedGearPiece: string) => {
+  // Retrieve data
+  const gearPieceModifierData: GearType = require(`../../data/modifiers/${selectedGearPiece}.json`);
 
-    // Retrieve data
-    const gearPieceModifierData: GearType = require(`../../data/modifiers/${selectedGearPiece}.json`);
+  gearPieceModifierData.modifierGroup.PREFIX = removeDuplicateLevels(
+    gearPieceModifierData.modifierGroup.PREFIX
+  );
+  gearPieceModifierData.modifierGroup.IMPLICIT = removeDuplicateLevels(
+    gearPieceModifierData.modifierGroup.IMPLICIT
+  );
 
-    gearPieceModifierData.modifierGroup.PREFIX = removeDuplicateLevels(gearPieceModifierData.modifierGroup.PREFIX)
-    gearPieceModifierData.modifierGroup.IMPLICIT = removeDuplicateLevels(gearPieceModifierData.modifierGroup.IMPLICIT)
+  // Return
+  return gearPieceModifierData;
+};
 
-    // Return
-    return gearPieceModifierData;
+export const getModifierGroupDisplayName = (
+  modifierGroup: (typeof groupTypes)[number]
+) => {
+  let displayName = "Unknown";
 
-}
+  if (data.vault_gear.modifier_groups[modifierGroup] != null) {
+    displayName = data.vault_gear.modifier_groups[modifierGroup];
+  }
+  return displayName;
+};
 
-export const getModifierGroupDisplayName = (modifierGroup: (typeof groupTypes)[number]) => {
-    let displayName = 'Unknown';
-
-    if (data.vault_gear.modifier_groups[modifierGroup] != null) {
-        displayName = data.vault_gear.modifier_groups[modifierGroup];
-    }
-    return displayName;
-}
-
-export const getExcludingModifiers = (modifiers: ModifierType[], modifier: ModifierType) => {
-    return modifiers
-        .filter((otherModifier) => otherModifier.group === modifier.group && otherModifier != modifier)
-        .map(otherModifier => getModifierData(otherModifier).name)
-}
+export const getExcludingModifiers = (
+  modifiers: ModifierType[],
+  modifier: ModifierType
+) => {
+  return modifiers
+    .filter(
+      (otherModifier) =>
+        otherModifier.group === modifier.group && otherModifier != modifier
+    )
+    .map((otherModifier) => getModifierData(otherModifier).name);
+};
 
 export const getModifierData = (modifier: ModifierType) => {
-    // Initiate variables
-    let modifierData = {
-        "name": "Unknown",
-        "description": '',
-        "color": "#ffffff"
-    };
+  // Initiate variables
+  let modifierData = {
+    name: "Unknown",
+    description: "",
+    color: "#ffffff",
+  };
 
-    const lang = data.vault_gear.modifiers
+  const lang = data.vault_gear.modifiers;
 
-    // Check if attribute exists
-    if (modifier.attribute in lang) {
-        modifierData = lang[modifier.attribute as keyof typeof lang];
-    }
+  // Check if attribute exists
+  if (modifier.attribute in lang) {
+    modifierData = lang[modifier.attribute as keyof typeof lang];
+  }
 
-    if (modifier.identifier in lang) {
-        modifierData = lang[modifier.identifier as keyof typeof lang];
-    }
+  if (modifier.identifier in lang) {
+    modifierData = lang[modifier.identifier as keyof typeof lang];
+  }
 
-    return modifierData;
-
-}
+  return modifierData;
+};
--- src/app/gear/page.tsx
+++ src/app/gear/page.tsx
@@ -1,117 +1,155 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import GearPiece from "@/components/gearPiece";
-import BackgroundImage from "@/components/backgroundImage"
+import BackgroundImage from "@/components/backgroundImage";
 import Title from "@/components/title";
 import data from "@/data/vault_gear.json";
-import s from './gear.module.scss'
+import s from "./gear.module.scss";
 import Modifier from "@/components/modifier";
-import { getExcludingModifiers, getGearPieceModifierData, getModifierGroupDisplayName, getModifierData } from "./functions";
+import {
+  getExcludingModifiers,
+  getGearPieceModifierData,
+  getModifierGroupDisplayName,
+  getModifierData,
+  ModifierType,
+  gearPieces,
+  groupTypes,
+} from "./functions";
 import Slider from "@/components/slider";
-export const gearPieces = ['sword', 'axe', 'helmet', 'chestplate', 'leggings', 'boots', 'idol_benevolent', 'shield', 'wand', 'magnet', 'jewel'] as const;
-export const groupTypes = ['PREFIX', 'SUFFIX', 'IMPLICIT', 'CRAFTED_PREFIX', 'CRAFTED_SUFFIX'] as const;
-
-export type TierType = { minLevel: number, maxLevel: number, weight: number, value: { min: number, max: number, step: number, levelChange?: number, minChance?: number, maxChance?: number, tooltipDisplayName?: string, registryKey?: string } }
-
-export type ModifierType = {
-    attribute: string,
-    group: string,
-    identifier: string,
-    tiers: TierType[]
-}
-
-export type GearType = {
-    modifierGroup: {
-        IMPLICIT: ModifierType[],
-        PREFIX: ModifierType[],
-        SUFFIX: ModifierType[],
-        CRAFTED_PREFIX: ModifierType[],
-        CRAFTED_SUFFIX: ModifierType[],
-    }
-}
-
 
 export default () => {
-    const [activeTab, setActiveTab] = useState<(typeof gearPieces)[number]>('sword')
-    const [level, setLevel] = useState(0)
-    const getGearPieceDisplayName = (gearPiece: (typeof gearPieces)[number]) => data.vault_gear.gear_pieces[gearPiece]
+  const [activeTab, setActiveTab] =
+    useState<(typeof gearPieces)[number]>("sword");
+  const [level, setLevel] = useState(0);
+  const getGearPieceDisplayName = (gearPiece: (typeof gearPieces)[number]) =>
+    data.vault_gear.gear_pieces[gearPiece];
 
-    const getTotalWeight = (modifierGroup: (typeof groupTypes)[number], modifiers: ModifierType[]) => {
+  const getTotalWeight = (
+    modifierGroup: (typeof groupTypes)[number],
+    modifiers: ModifierType[]
+  ) => {
+    // Initiate variables
+    var totalWeight = 0;
 
-        // Initiate variables
-        var totalWeight = 0;
-
-        // Guard clause
-        if (!['IMPLICIT', 'PREFIX', 'SUFFIX'].includes(modifierGroup)) {
-            return totalWeight;
-        }
-
-        // Map modifiers
-        modifiers.map((modifier) => {
-
-            // Guard clauses
-            if (modifier.tiers.length > 0
-                && !(modifierGroup === 'IMPLICIT' && getExcludingModifiers(modifiers, modifier).length === 0)
-                && modifier.tiers.some((tier) => (tier?.minLevel <= level && level <= (tier.maxLevel < 0 ? 100 : tier.maxLevel)))) {
-
-                // Initiate variables
-                const availableTiers = modifier.tiers.filter(tier => tier.minLevel <= level && (tier.maxLevel === -1 || tier.maxLevel >= level));
-
-                // Loop through tiers
-                availableTiers.map((tier) => {
-                    // Increase weight
-                    totalWeight += tier.weight;
-                })
-            }
-        })
-
-        return totalWeight;
+    // Guard clause
+    if (!["IMPLICIT", "PREFIX", "SUFFIX"].includes(modifierGroup)) {
+      return totalWeight;
     }
 
-    return <>
-        <BackgroundImage backgroundType="untextured" />
-        <div className={s.container}>
-            <Title icon="/images/chestplate.png" title="Vault Gear" />
-            <div className={s.gearPieces}>
-                {gearPieces.map((gearPiece, index) => (
-                    <GearPiece key={index} name={gearPiece} isOddChild={index % 2 === 0} active={activeTab === gearPiece} onClick={() => setActiveTab(gearPiece)} />
-                ))}
-            </div>
-            <div className={s.content}>
-                <div className={s.gearHeader}>
-                    <h2 className={s.gearTitle}>{`${getGearPieceDisplayName(activeTab)}`}</h2>
-                    <Slider onChange={e => setLevel(e)} />
-                </div>
-                <div className={s.groups}>
-                    {Object.entries(getGearPieceModifierData(activeTab).modifierGroup).map(([modifierGroup, modifiers]) =>
-                        getModifierGroupDisplayName(modifierGroup as (typeof groupTypes)[number]) !== "Unknown"
-                        && modifiers.length > 0
-                        && modifiers.some((modifier) => modifier.tiers.some((tier) => tier.minLevel <= level))
-                        && (
-                            <div key={modifierGroup}>
-                                <h3 className={s.subtitle}>{getModifierGroupDisplayName(modifierGroup as (typeof groupTypes)[number])}</h3>
-                                {['PREFIX', 'SUFFIX'].includes(modifierGroup) &&
-                                    <div className={s.flavourText}>A gear piece with an odd amount of modifier slots has a 50% chance at rolling either a prefix or suffix.</div>
-                                }
-                                {modifiers.map((modifier) => modifier.tiers.length > 0
-                                    && modifier.tiers.some((tier) => (tier?.minLevel <= level && level <= (tier.maxLevel < 0 ? 100 : tier.maxLevel) && tier.weight > 0)) && (
-                                        <Modifier
-                                            key={modifier.identifier}
-                                            gearPiece={activeTab}
-                                            modifierGroup={modifierGroup}
-                                            modifier={modifier}
-                                            langData={getModifierData(modifier)}
-                                            level={level}
-                                            excludingModifiers={getExcludingModifiers(modifiers, modifier)}
-                                            totalWeight={getTotalWeight(modifierGroup as (typeof groupTypes)[number], modifiers)}
-                                        />
-                                    ))
-                                }
-                            </div>
+    // Map modifiers
+    modifiers.map((modifier) => {
+      // Guard clauses
+      if (
+        modifier.tiers.length > 0 &&
+        !(
+          modifierGroup === "IMPLICIT" &&
+          getExcludingModifiers(modifiers, modifier).length === 0
+        ) &&
+        modifier.tiers.some(
+          (tier) =>
+            tier?.minLevel <= level &&
+            level <= (tier.maxLevel < 0 ? 100 : tier.maxLevel)
+        )
+      ) {
+        // Initiate variables
+        const availableTiers = modifier.tiers.filter(
+          (tier) =>
+            tier.minLevel <= level &&
+            (tier.maxLevel === -1 || tier.maxLevel >= level)
+        );
+
+        // Loop through tiers
+        availableTiers.map((tier) => {
+          // Increase weight
+          totalWeight += tier.weight;
+        });
+      }
+    });
+
+    return totalWeight;
+  };
+
+  return (
+    <>
+      <BackgroundImage backgroundType="untextured" />
+      <div className={s.container}>
+        <Title icon="/images/chestplate.png" title="Vault Gear" />
+        <div className={s.gearPieces}>
+          {gearPieces.map((gearPiece, index) => (
+            <GearPiece
+              key={index}
+              name={gearPiece}
+              isOddChild={index % 2 === 0}
+              active={activeTab === gearPiece}
+              onClick={() => setActiveTab(gearPiece)}
+            />
+          ))}
+        </div>
+        <div className={s.content}>
+          <div className={s.gearHeader}>
+            <h2 className={s.gearTitle}>{`${getGearPieceDisplayName(
+              activeTab
+            )}`}</h2>
+            <Slider onChange={(e) => setLevel(e)} />
+          </div>
+          <div className={s.groups}>
+            {Object.entries(
+              getGearPieceModifierData(activeTab).modifierGroup
+            ).map(
+              ([modifierGroup, modifiers]) =>
+                getModifierGroupDisplayName(
+                  modifierGroup as (typeof groupTypes)[number]
+                ) !== "Unknown" &&
+                modifiers.length > 0 &&
+                modifiers.some((modifier) =>
+                  modifier.tiers.some((tier) => tier.minLevel <= level)
+                ) && (
+                  <div key={modifierGroup}>
+                    <h3 className={s.subtitle}>
+                      {getModifierGroupDisplayName(
+                        modifierGroup as (typeof groupTypes)[number]
+                      )}
+                    </h3>
+                    {["PREFIX", "SUFFIX"].includes(modifierGroup) && (
+                      <div className={s.flavourText}>
+                        A gear piece with an odd amount of modifier slots has a
+                        50% chance at rolling either a prefix or suffix.
+                      </div>
+                    )}
+                    {modifiers.map(
+                      (modifier) =>
+                        modifier.tiers.length > 0 &&
+                        modifier.tiers.some(
+                          (tier) =>
+                            tier?.minLevel <= level &&
+                            level <=
+                              (tier.maxLevel < 0 ? 100 : tier.maxLevel) &&
+                            tier.weight > 0
+                        ) && (
+                          <Modifier
+                            key={modifier.identifier}
+                            gearPiece={activeTab}
+                            modifierGroup={modifierGroup}
+                            modifier={modifier}
+                            langData={getModifierData(modifier)}
+                            level={level}
+                            excludingModifiers={getExcludingModifiers(
+                              modifiers,
+                              modifier
+                            )}
+                            totalWeight={getTotalWeight(
+                              modifierGroup as (typeof groupTypes)[number],
+                              modifiers
+                            )}
+                          />
                         )
                     )}
-                </div>
-            </div>
+                  </div>
+                )
+            )}
+          </div>
         </div>
+      </div>
     </>
-}
+  );
+};
